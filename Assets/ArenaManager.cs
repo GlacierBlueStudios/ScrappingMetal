5d7e30df7a275ba9f4bc15da68ccb927e0b8263b
ctooley21 <christian.tooley@uky.edu>
2018-06-23 18:26:02 -0400

    Add basic ArenaManager

---
+++ Assets/ArenaManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaManager : MonoBehaviour {
+
+    public int teamsLeft;
+    public int playersLeft;
+
+    public long timeTillNextDrop;
+
+    /**
+     * Placeholder till we have networking setup and I can actually do some networking magic to detect players connected and alive.
+    **/
+
+	void Start () {
+        teamsLeft = 0;
+        playersLeft = 0;
+        timeTillNextDrop = 120;
+	}
+	
+	void Update () {
+		
+	}
+}
