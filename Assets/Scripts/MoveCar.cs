c010159fff9ecf066c61451b2f5c28059aa4369b
ctooley21 <christian.tooley@uky.edu>
2018-06-23 21:48:16 -0400

    Add basic networking

---
+++ Assets/Scripts/MoveCar.cs
@@ -0,0 +1,27 @@
+﻿using BeardedManStudios.Forge.Networking.Generated;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCar : MoveCarBehavior
+{
+	void Update ()
+    {
+
+        if(!networkObject.IsServer)
+        {
+            transform.position = networkObject.position;
+            return;
+        }
+
+        transform.position += new Vector3(
+            Input.GetAxis("Horizontal"),
+            0,
+            Input.GetAxis("Vertical")
+        ) * Time.deltaTime * 15.0f;
+
+        Debug.Log(Input.GetAxis("Horizontal") + " - " + Input.GetAxis("Vertical"));
+
+        networkObject.position = transform.position;
+    }
+}
